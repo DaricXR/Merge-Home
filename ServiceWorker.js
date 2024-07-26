583d82c35ceb5f85320d8194cea80464006b3fd1
HamzaMushtaq <hamzamushtaqtt@gmail.com>
2024-07-27 04:16:32 +0500

    fdfd

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GTap Studio inc-Merge Room-0.1.0";
+const contentToCache = [
+    "Build/Merge-Home.loader.js",
+    "Build/Merge-Home.framework.js",
+    "Build/Merge-Home.data",
+    "Build/Merge-Home.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
